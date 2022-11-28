133be9ddf8a176c71446f80e09dbd5a353733dcd
Ashish Birajdar <ashishbirajdar2001@gmail.com>
2022-11-28 17:11:01 +0530

    User model and routes added

--- Cult-terra/server/index.js
+++ Cult-terra/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const userRoute = require("./routes/user.route.js")
 
 const app = express();
 mongoose.connect('mongodb://127.0.0.1/cultterra')
@@ -10,5 +11,6 @@ db.once('open', () => console.log('Database Connected...'));
  
 app.use(cors());
 app.use(express.json());
+app.use("/user",userRoute)
  
 app.listen(5000, ()=> console.log('Server up and running...'));
--- Cult-terra/server/routes/user.route.js
+++ Cult-terra/server/routes/user.route.js
@@ -1,1 +1,49 @@
-//user route
+const express = require("express")
+const mongoose = require("mongoose")
+const bcyrpt = require("bcryptjs")
+const Users = require("../models/User.js")
+
+const router = express.Router()
+const salt = "$2a$10$llw0G6IyibUob8h5XRt9xuRczaGdCm/AiV6SSjf5v78XS824EGbh"
+
+router.post("/adduser", async (req,res) =>{
+
+    try {
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = bcyrpt.hashSync(req.body.password,salt);
+    
+    const user = new Users({name:name,email:email,password:password})
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+
+router.post('/validateuser',async (req,res) =>{
+    const email= req.body.email;
+    const password = req.body.password;
+    try {
+        let user = await Users.findOne({email:email});
+        if(!user)
+        {
+            return res.status(400).json({message:"Email or password is invalid!"})
+        }
+
+        const isMatch = await bcyrpt.compare(password,user.password)
+        if(!isMatch)
+        {
+            return res.status(400).json({message:"Email or password is invalid!"})
+        }
+        res.json(user)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+module.exports = router

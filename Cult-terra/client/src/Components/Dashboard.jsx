44986feb9e5b1732ab2b637b68a16b3dfeed3181
Ashish Birajdar <ashishbirajdar2001@gmail.com>
2022-11-29 21:18:30 +0530

    dashboard frontend added

--- Cult-terra/client/src/Components/Dashboard.jsx
+++ Cult-terra/client/src/Components/Dashboard.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import ModalUnbooked from './ModalUnbooked'
+import ModalBooked from './ModalBooked'
+import axios from "axios"
 
 const DashBoard = (props) => {
+  const [rooms, setRooms] = useState([]);
+  const [showUnbookedModal, setShowUnbookedModal] = useState(false);
+  const [showbookedModal, setShowbookedModal] = useState(false);
+
+  useEffect(() => {
+    getRooms();
+  }, []);
+
+  const getRooms = async () =>{
+    const response = await axios.get("http://localhost:5000/rooms/getall");
+    setRooms(response.data);
+  }
+
   return (
     <div>
       <h1 className='text-slate-500 text-3xl font-bold'>Dashborad</h1>
@@ -12,6 +29,21 @@ const DashBoard = (props) => {
       >
         click
       </button>
+      <h1>Book Room</h1>
+      <div className="min-h-screen flex items-center">
+        <div className="flex-1 max-w-4xl mx-auto ">
+          <ul className="grid grid-cols-8 grid-rows-5 gap-8">
+            {rooms.map((room,index)=>(
+              <button key={room._id} className={`${ room.status=== 'booked' ? " bg-green-400 " :" bg-red-400 "} rounded-lg shadow-xl hover:shadow-lg text-center align-text-top w-24 h-24 font-bold`} type="button"
+                onClick={() => {
+                    room.status==='unbooked'?setShowUnbookedModal(true):setShowbookedModal(true)
+                }}>{room.roomNo}</button> 
+            ))}
+          </ul>
+        </div>
+      </div>
+      {showUnbookedModal && <ModalUnbooked setOpenModal={setShowUnbookedModal}></ModalUnbooked> }
+      {showbookedModal && <ModalBooked setOpenModal={setShowbookedModal}></ModalBooked> }
     </div>
   );
 }

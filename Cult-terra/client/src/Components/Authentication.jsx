f9b7dab9d614b951c8834e40c9233e938208260d
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-11-29 12:40:18 +0530

    Tailwind installed

--- Cult-terra/client/src/Components/Authentication.jsx
+++ Cult-terra/client/src/Components/Authentication.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
 
 const Authentication = (props) => {
+
+  const [isOnLogin,setIsOnLogin] = useState(true); 
+
   return (
     <div>
-        <h1>Authentication</h1>
+      <h1 className="text-slate-500 text-4xl font-bold">Authentication</h1>
+      {
+        isOnLogin ?
+        <LogIn setIsOnLogin={setIsOnLogin}/> :
+        <SignUp setIsOnLogin={setIsOnLogin}/>
+      }
       <button
+        className="bg-cyan-400 py-1 px-5 rounded-lg active:bg-cyan-700 active:text-white"
         onClick={() => {
           props.setIsOnDashboard((isOnDashboard) => !isOnDashboard);
         }}
